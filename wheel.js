1a0fe21cfd3b5d6ddd6e258da0a5a646424817b2
Josh <joshbell635@gmail.com>
2025-07-05 10:16:25 -0700

    Icon follows wheel slice

--- wheel.js
+++ wheel.js
@@ -9,11 +9,13 @@ class Wheel {
 	// Draw the slices of the wheel
 	draw(context, angle) {
 		const sliceCount = this.colors.length;
+		const anglePerSlice = 2 * Math.PI / sliceCount
+
 		// Create Colored Wheel
 		for (let i = 0; i < sliceCount; i++) {
 			// Gather data for the angles of the slices
-			const startAngle = angle + 2 * Math.PI * (i / sliceCount)
-			const endAngle = angle + 2 * Math.PI * ((i + 1) / sliceCount)
+			const startAngle = angle + i * anglePerSlice
+			const endAngle = startAngle + anglePerSlice
 
 			// Begin drawing the slices
 			context.beginPath()
@@ -21,6 +23,24 @@ class Wheel {
 			context.arc(center.x, center.y, this.radius, startAngle, endAngle)
 			context.fillStyle = this.colors[i]
 			context.fill();
+
+			// Add image
+			if (this.colors[i]=="violet"){
+				context.save();
+				const image = new Image();
+				image.src = "Characters/mario_icon.png"
+				const iconSliceHeight = -this.radius * 0.75
+
+				// Translate to the center of the wheel then move to center of the slice
+    			context.translate(this.center.x, this.center.y);
+    			context.rotate(startAngle + anglePerSlice / 2);
+
+				// Move icon down and draw the icon
+    			context.translate(0, iconSliceHeight);
+    			const ICON_SIZE = 150;
+    			context.drawImage(image, -ICON_SIZE / 2, -ICON_SIZE / 2, ICON_SIZE, ICON_SIZE);
+   				context.restore();
+			}
 		}
 
 		// Create slice borders
